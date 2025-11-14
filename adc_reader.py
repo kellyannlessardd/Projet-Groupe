43f64d41847cad1bdfc2a574cbf97baad3aeeccc
Artem <alavr011@uottawa.ca>
2025-11-14 18:34:09 -0500

    Added 2 function(read_potentiometers;read_switch)

--- adc_reader.py
+++ adc_reader.py
@@ -7,6 +7,10 @@ pot_y = ADC(27)
 # Interrupteur (stylo)
 switch = Pin(16, Pin.IN, Pin.PULL_UP)
 
+# --- Etat mémoire du stylo (False = haut, True = bas) ---
+pen_state = False
+last_switch_value = 1
+
 def read_potentiometers():
     """
     Lit les deux potentiomètres et retourne X et Y.
@@ -18,8 +22,17 @@ def read_potentiometers():
 
 def read_switch():
     """
-    Retourne l'état du stylo.
-    True  -> stylo en bas
-    False -> stylo en haut
+    Modifie l'état du stylo à chaque pression sur l'interrupteur.
+    Retourne l'état actuel du stylo : True = bas, False = haut.
     """
-    return switch.value() == 0
+    global pen_state, last_switch_value
+
+    current_value = switch.value()
+
+    if last_switch_value == 1 and current_value == 0:
+        pen_state = not pen_state  # Toggle de l'état
+
+    # Mise à jour du précédent état
+    last_switch_value = current_value
+
+    return pen_state
